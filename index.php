42059985c005923891bc71bbb749f69575cd66c1
E-S-C-USER <edrys.Chalopin@ism-antony.org>
2026-09-11 10:45:06 +0000

    css prime

--- index.php
+++ index.php
@@ -355,5 +355,18 @@ if(file_exists("content.html"))
 
 </footer>
 
+<div id="cursor-glow"></div>
+
+<script>
+const glow = document.getElementById("cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+
+});
+</script>
+
 </body>
 </html>
